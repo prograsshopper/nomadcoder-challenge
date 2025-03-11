1d8c93b45cabb9cc3eb49604d593636ac03a982f
prograsshopper <tjalwled12@gmail.com>
2025-03-11 23:31:01 +0900

    challenge 3: custom list filter and search fields

--- tweets/admin.py
+++ tweets/admin.py
@@ -2,6 +2,25 @@ from django.contrib import admin
 from .models import Tweet, Like
 
 
+class MuskFilter(admin.SimpleListFilter):
+    title = "Filter by Musk"
+
+    parameter_name = "musk"
+
+    def lookups(self, request, model_admin):
+        return [
+            ("contain", "Contain Elon Musk"),
+            ("exclude", "Exclude Elon Musk"),
+        ]
+
+    def queryset(self, request, tweets):
+        word = self.value()
+        if word == 'contain':
+            return tweets.filter(payload__contains="Elon Musk")
+        if word == 'exclude':
+            return tweets.exclude(payload__contains="Elon Musk")
+
+
 @admin.register(Tweet)
 class TweetAdmin(admin.ModelAdmin):
     list_display = ('payload', 'like_count')
@@ -10,8 +29,13 @@ class TweetAdmin(admin.ModelAdmin):
         return obj.likes.count()
 
     like_count.short_description = 'Like Count'
+    search_fields = ["payload", "user__username"]
+    list_filter = [
+        "created_at", MuskFilter
+    ]
 
 
 @admin.register(Like)
 class LikeAdmin(admin.ModelAdmin):
-    pass
+    search_fields = ["user__username"]
+    list_filter = ["created_at"]
